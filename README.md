216d4e100c1bc5e5fc51c02ae959ac31cd005595
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2019-06-21 17:26:16 +0900

    Qiitaいいねランキング通知Bot作成

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
+### 参考
+- Docker環境作成
+[Dockerで Serverless Framework 環境構築](https://qiita.com/seiichi_akiba/items/d42bda576f3fc6ac5117)
+
+- Slackへ特定の文字列でのリンクの作成
+[Slackのメッセージにて任意の文字列にリンクを埋め込むことはできますか？解決済](https://teratail.com/questions/80285)
+
+### Docker環境
+
+#### 注意点  
+※Dockerfileにも記載はしているが、うまくいかなかった場合
+
+serverless-python-requirementsをインストールする際、マウントしたディレクトリの中であるとエラーになるので、マウント外のフォルダに移動してからインストールコマンド実行  
+`$ npm install --save serverless-python-requirements`
+
+### コマンド
+
 - Dockerコンテナに入る  
 `$ docker-compose exec serverless sh`
+
+- Serverless雛形作成(Python)  
+`$ sls create -t aws-python`
+
+- デプロイ  
+`$ sls deploy`
+
+- ローカルで動作テスト  
+`$ sls invoke local -f ファンクション名`
---
+++ src/QiitaRanking/qiita_ranking.py
@@ -0,0 +1,37 @@
+import json
+import requests
+from datetime import datetime
+from pytz import timezone
+import os
+
+
+def get_ranking(event, context):
+    type = 'weekly'
+    date = datetime.now(timezone('Asia/Tokyo')).strftime('%Y-%m-%d')
+    url = os.environ['qiitaScraipingUrl'] + type + '/' + date
+    response = requests.get(url).json()
+
+    data_list = response['data']
+    msg = '*週間いいねランキング* \n\n'
+    count = 1
+    for data in data_list:
+        msg += str(count) + '位：<' + data['url'] + '|' + data['title'] + '> \n'
+        count += 1
+
+    send_data = {
+        'text': msg
+    }
+    slack_url = os.environ['slackWebhookUrl']
+    slack_send_data = {
+        "payload": json.dumps(send_data)
+    }
+    requests.post(slack_url, slack_send_data)
+
+
+# if __name__ == '__main__':
+#     os.environ['qiitaScraipingUrl'] = 'https://us-central1-qiita-trend-web-scraping.cloudfunctions.net/qiitaScraiping/'
+#     os.environ['slackWebhookUrl'] = ''
+#     get_ranking(None, None)
+
+
+
